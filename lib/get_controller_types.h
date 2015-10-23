787bdcac1b5bee1a87b176b5d7c5872c610337dc
Jacob Lannen <jacob.lannen@gmail.com>
2015-10-23 21:24:10 +1100

    Create get_controller_types.h

---
+++ lib/get_controller_types.h
@@ -0,0 +1,45 @@
+/*
+ * get_controller_types.h
+ *
+ * Code generation for function 'get_controller'
+ *
+ * C source code generated on: Tue Oct 20 19:40:12 2015
+ *
+ */
+
+#ifndef __GET_CONTROLLER_TYPES_H__
+#define __GET_CONTROLLER_TYPES_H__
+
+/* Include files */
+#include "rtwtypes.h"
+
+/* Type Definitions */
+#ifndef typedef_b_struct_T
+#define typedef_b_struct_T
+typedef struct
+{
+    double M;
+    double l;
+    double m;
+    double r;
+    double I;
+    double J;
+    double g;
+} b_struct_T;
+#endif /*typedef_b_struct_T*/
+#ifndef typedef_struct_T
+#define typedef_struct_T
+typedef struct
+{
+    double M;
+    double m;
+    double l;
+    double r;
+    double I;
+    double J;
+    double g;
+} struct_T;
+#endif /*typedef_struct_T*/
+
+#endif
+/* End of code generation (get_controller_types.h) */
