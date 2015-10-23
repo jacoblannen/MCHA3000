6b10c6e926b4adf599628cebe7429d2f64fcc889
Jacob Lannen <jacob.lannen@gmail.com>
2015-10-23 21:24:38 +1100

    Create get_params.h

---
+++ lib/get_params.h
@@ -0,0 +1,22 @@
+/*
+ * get_params.h
+ *
+ * Code generation for function 'get_params'
+ *
+ * C source code generated on: Tue Oct 20 19:40:12 2015
+ *
+ */
+
+#ifndef __GET_PARAMS_H__
+#define __GET_PARAMS_H__
+/* Include files */
+#include <math.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "get_controller_types.h"
+
+/* Function Declarations */
+extern void get_params(b_struct_T *parameters);
+#endif
+/* End of code generation (get_params.h) */
